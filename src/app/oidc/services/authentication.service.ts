fdc56d020aa7fdf7f451d0fb5bda4925bae72fda
tesla <kela256@gmail.com>
2021-09-10 13:39:16 +0300

    change oidc settings

--- src/app/oidc/services/authentication.service.ts
+++ src/app/oidc/services/authentication.service.ts
@@ -4,8 +4,6 @@ import { UserManagerSettings } from '../models';
 
 @Injectable()
 export class AuthenticationService {
-  constructor() {}
-
   isUserDefined = false;
   private _user: User | null;
   private _userManager: UserManager;
@@ -23,12 +21,12 @@ export class AuthenticationService {
   }
 
   startAuthentication(): Promise<void> {
-    this.GetUserManager();
+    this.getUserManager();
     return this._userManager.signinRedirect();
   }
 
   completeAuthentication() {
-    this.GetUserManager();
+    this.getUserManager();
     return this._userManager.signinRedirectCallback().then((user) => {
       this._user = user;
       this.isUserDefined = true;
@@ -37,35 +35,35 @@ export class AuthenticationService {
 
 
   startLogout(): Promise<void> {
-    this.GetUserManager();
+    this.getUserManager();
     return this._userManager.signoutRedirect();
   }
 
   completeLogout() {
-    this.GetUserManager();
+    this.getUserManager();
     this._user = null;
     return this._userManager.signoutRedirectCallback();
   }
 
 
-  async silentSignInAuthentication() {
-    this.GetUserManager();
+  silentSignInAuthentication() {
+    this.getUserManager();
     return this._userManager.signinSilentCallback();
   }
 
 
-  private GetUserManager() {
+  private getUserManager() {
     if (!this._userManager) {
       const userManagerSettings: UserManagerSettings =
         new UserManagerSettings();
 
       //set up settings
-      userManagerSettings.authority = ''; //website that responsible for authorization
-      userManagerSettings.client_id = ''; //uniqe name to identify the project
-      userManagerSettings.response_type = 'code'; //desired authorization processing flow - for angular is sutible code flow
-      userManagerSettings.scope = ''; //specify the access privileges, specifies the information returned about the authenticated user.
-      //Standard Scopes: openid, profile 
-
+      userManagerSettings.authority = 'http:/server-url'; //website that responsible for Authentication
+      userManagerSettings.client_id = 'angular'; //uniqe name to identify the project
+      userManagerSettings.response_type = 'code'; //desired Authentication processing flow - for angular is sutible code flow
+      //specify the access privileges, specifies the information returned about the authenticated user.
+      userManagerSettings.scope = 'openid profile'; 
+      
       userManagerSettings.redirect_uri = 'http://localhost:4200/login-callback'; //start login process
       userManagerSettings.post_logout_redirect_uri = 'http://localhost:4200/logout-callback'; //start logout process
 
@@ -74,7 +72,7 @@ export class AuthenticationService {
 
       userManagerSettings.userStore = new WebStorageStateStore({
         store: window.localStorage,
-      }); // store information about authorization in localStorage
+      }); // store information about Authentication in localStorage
 
       this._userManager = new UserManager(userManagerSettings);
 
